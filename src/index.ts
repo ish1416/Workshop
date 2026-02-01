6d7c38f16bc4a300649a8ab8b45b32309d9fd243
ish1416 <ish1442006@gmail.com>
2026-02-01 20:24:10 +0530

    refactor: improve index file with logger and constants

--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,19 @@
 import dotenv from 'dotenv';
 import { Server } from './server';
+import { Logger } from './logger';
+import { CONFIG } from './constants';
 
 dotenv.config();
 
-const port = parseInt(process.env.PORT || '3000');
+const port = parseInt(process.env.PORT || CONFIG.DEFAULT_PORT.toString());
 const server = new Server(port);
 
 // Graceful shutdown
 process.on('SIGINT', async () => {
-  console.log('Shutting down server...');
+  Logger.info('Shutting down server...');
   await server.stop();
   process.exit(0);
 });
 
+Logger.info('Starting Express TypeScript server...');
 server.start();
